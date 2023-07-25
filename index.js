30025e737fef8e1248da85d7bfa844dc490b6738
James Webster <james.webster@freetrade.io>
2023-07-25 22:02:03 +1000

    get this working

---
+++ index.js
@@ -0,0 +1,17 @@
+const Parser = require('tree-sitter')
+const MessageTemplates = require('./bindings/node')
+
+const parser = new Parser()
+parser.setLanguage(MessageTemplates)
+
+const sourceCode = 'User {username} logged in from {ip_address} {@somedata}'
+const tree = parser.parse(sourceCode)
+
+console.log(tree.rootNode.toString())
+// console.log(tree.rootNode.child(0).text)
+for (const child of tree.rootNode.children) {
+    console.log(child.type)
+    console.log(child.text)
+    console.log(child.childCount)
+    console.log()
+}
