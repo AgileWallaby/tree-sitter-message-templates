733321b1dc5497b5d980c657f83079f25b1d6183
James Webster <jim@agilewallaby.net>
2023-07-30 22:44:39 +1000

    Added typescript and jest dependencies.

---
+++ tree-sitter-message-templates/tests/test.ts
@@ -0,0 +1,17 @@
+import Parser from 'tree-sitter'
+import MessageTemplates from '@agilewallaby/tree-sitter-message-templates'
+
+const parser = new Parser()
+parser.setLanguage(MessageTemplates)
+
+const sourceCode = 'User {username} logged in from {ip_address} {@somedata}'
+const tree = parser.parse(sourceCode)
+
+console.log(tree.rootNode.toString())
+// console.log(tree.rootNode.child(0).text)
+for (const child of tree.rootNode.children) {
+    console.log(child.type)
+    console.log(child.text)
+    console.log(child.childCount)
+    console.log()
+}
